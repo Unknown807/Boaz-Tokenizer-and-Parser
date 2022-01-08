c257076719d908b444b399a8a0af0beda7861f4c
Unknown807 <milvovangveric@gmail.com>
2022-01-08 20:59:00 +0000

    Added error messages when exceptions trigger

--- analyser.py
+++ analyser.py
@@ -10,8 +10,20 @@ UNARY_OP = ("-", "!")
 
 #------------------------------------------------------------------------
 
-class ParserException(Exception): pass
-class TokenizeException(Exception): pass
+class ParserException(Exception):
+    def __init__(self, _type, token):
+        self.type = _type
+        self.token = token
+    
+    def __str__(self):
+        return "Something wrong with the last token or its type - TYPE:{}, TOKEN:{}".format(self.type, self.token)
+        
+class TokenizeException(Exception):
+    def __init__(self, token):
+        self.token = token
+
+    def __str__(self):
+        return "Error trying to create a token from the character/string: {}".format(self.token)
 
 #------------------------------------------------------------------------
 
@@ -43,12 +55,12 @@ class Tokenizer:
         token_str = char
         i+=1
         while ( i < end ):
-            if ((cls.CODE[i]+" ").isspace() or cls.CODE[i] in (",", ";")):
+            if ((cls.CODE[i]+" ").isspace() or cls.CODE[i] in cls.LITERALS+(":",)):
                 break
 
             token_str += cls.CODE[i]
             i+=1
-        i=i-1 # to tokenize the possible ',' or ';' next iter
+        i=i-1 # to tokenize the possible ',' or ';' or ')' next iter
 
         return (token_str, i)
 
@@ -78,18 +90,18 @@ class Tokenizer:
             elif ( char.isdigit() ):
                 token_str, i = cls.gather_chars(i, char, end)
                 if (not token_str.isdigit()):
-                    raise TokenizeException
+                    raise TokenizeException(token_str)
 
                 cls.TOKENS.append( ("INT_CONST", token_str) )
 
             # check if character is a single character enclosed with "
             elif ( char == '"' ):
+                token_str = char+cls.CODE[i+1]+cls.CODE[i+2]
                 if ( cls.CODE[i+1] != '"' and cls.CODE[i+2] == '"' ):
-                    token_str = char+cls.CODE[i+1]+cls.CODE[i+2]
                     cls.TOKENS.append( ("CHAR_CONST", token_str))
                     i+=2
                 else:
-                    raise TokenizeException
+                    raise TokenizeException(token_str)
 
             # the rest are symbols, 1 or 2 character literals
             else:
@@ -104,7 +116,7 @@ class Tokenizer:
                         cls.TOKENS.append( ("SYMBOL", token_str))
                     else:
                         # can't be tokenised, most likely error
-                        raise TokenizeException
+                        raise TokenizeException(token_str)
             i+=1
 
     @classmethod
@@ -115,13 +127,45 @@ class Tokenizer:
 
 #------------------------------------------------------------------------
 
+class ParseTreeNode:
+    def __init__(self, token, level):
+        self.token = token
+        self.depth = level
+        self.current_child = 0
+        self.children = []
+
+    def get_current_child(self):
+        return self.children[self.current_child]
+
+    # def add_node(self, node):
+    #     pass
+
+class ParseTree:
+    def __init__(self, _type):
+        self.intended_type = _type
+        self.root = None
+        self.current_depth = -1
+
+    def add_node(self, token):
+        if self.root == None:
+            self.current_depth+=1
+            self.root = ParseTreeNode(token, self.current_depth)
+        else:
+            current_node = self.root.get_current_child()
+            for i in range(self.current_depth):
+                current_node = current_node.get_current_child()
+            
+
+
+#------------------------------------------------------------------------
+
 class Parser:
     
-    SYMBOL_TABLE = {}
+    ID_TYPES = {}
 
     @classmethod
     def is_identifier_declared(cls, token):
-        return token in cls.SYMBOL_TABLE.keys()
+        return token in cls.ID_TYPES.keys()
 
     @classmethod
     def is_valid_identifier(cls, token):
@@ -141,21 +185,21 @@ class Parser:
         _type, id_token = Tokenizer.get_next_token()
 
         if (program_token != "program" or _type != "IDENTIFIER" or not cls.is_valid_identifier(id_token)):
-            raise ParserException
+            raise ParserException(_type, program_token+"or "+id_token)
 
         cls.parse_var_decs()
         cls.parse_statements()
 
     @classmethod
     def parse_var_decs(cls):
-        _, token = Tokenizer.get_next_token()
+        _type, token = Tokenizer.get_next_token()
         if (token == "begin"):
             return
         elif (token in ("int", "char")):
             cls.parse_var_list(token)
             cls.parse_var_decs()
         else:
-            raise ParserException
+            raise ParserException(_type, token)
     
     @classmethod
     def parse_var_list(cls, var_type):
@@ -167,21 +211,21 @@ class Parser:
         _type, token = Tokenizer.get_next_token()
 
         if (_type != "IDENTIFIER"):
-            raise ParserException
+            raise ParserException(_type,  token)
 
         if (not cls.is_valid_identifier(token)):
-            raise ParserException
+            raise ParserException(_type, token)
 
         # set the type of the variable for later semantic analysis
-        cls.SYMBOL_TABLE[token] = var_type
+        cls.ID_TYPES[token] = var_type
 
-        _, token = Tokenizer.get_next_token()
+        _type, token = Tokenizer.get_next_token()
         if (token == ";"):
             return
         elif (token == ","):
             cls.parse_var_list(var_type)
         else:
-            raise ParserException
+            raise ParserException(_type, token)
 
     @classmethod
     def parse_statements(cls):
@@ -193,7 +237,7 @@ class Parser:
         - while statement
         '''
 
-        _, token = Tokenizer.get_next_token()
+        _type, token = Tokenizer.get_next_token()
 
         if (token in ("end", "od", "fi")):
             return
@@ -201,61 +245,65 @@ class Parser:
             cls.parse_statements()
             return
         elif (token == "while"):
-            cls.parse_while()
+            cls.parse_while(ParseTree("bool"))
         elif (token == "if"):
-            cls.parse_if()
+            cls.parse_if(ParseTree("bool"))
         elif (token == "print"):
             cls.parse_expression()
         elif (cls.is_valid_identifier(token) and cls.is_identifier_declared(token)):
             # assignment statement starts with a valid identifier
             # otherwise it is incorrect
-            cls.parse_assign()
+            cls.parse_assign(ParseTree(cls.ID_TYPES[token]))
         else:
-            raise ParserException
+            raise ParserException(_type, token)
         
         cls.parse_statements()
 
     @classmethod
-    def parse_if(cls):
-        cls.parse_expression()
+    def parse_if(cls, parse_tree):
+        cls.parse_expression(parse_tree)
         cls.parse_statements()
 
-        _, token = Tokenizer.get_next_token()
+        _type, token = Tokenizer.get_next_token()
         if (token != ";"):
-            raise ParserException
+            raise ParserException(_type, token)
 
     @classmethod
-    def parse_assign(cls):
-        _, token = Tokenizer.get_next_token()
+    def parse_assign(cls, parse_tree):
+        _type, token = Tokenizer.get_next_token()
         
         if (token == ":="):
-            cls.parse_expression()
+            cls.parse_expression(parse_tree)
         else:
-            raise ParserException
+            raise ParserException(_type, token)
 
     @classmethod
-    def parse_while(cls):
-        cls.parse_expression()
+    def parse_while(cls, parse_tree):
+        cls.parse_expression(parse_tree)
         cls.parse_statements()
-        _, token = Tokenizer.get_next_token()
+        _type, token = Tokenizer.get_next_token()
         if (token != ";"):
-            raise ParserException
+            raise ParserException(_type, token)
 
     @classmethod
-    def parse_expression(cls):
-        cls.parse_term()
+    def parse_expression(cls, parse_tree=None):
+        cls.parse_term(parse_tree)
 
-        _, token = Tokenizer.get_next_token()
+        _type, token = Tokenizer.get_next_token()
+
         if (token in (";", "then", "do", ")")):
             return
         elif (token in ARITHMETIC_OP+BOOLEAN_OP+RELATIONAL_OP):
             cls.parse_expression()
         else:
-            raise ParserException
+            raise ParserException(_type, token)
 
     @classmethod
-    def parse_term(cls):
+    def parse_term(cls, parse_tree=None):
         _type, token = Tokenizer.get_next_token()
+
+        # if (parse_tree is not None):
+        #     parse_tree.add_node(token)
 
         if (_type != "INT_CONST" and _type != "CHAR_CONST"):
             if (token == "("):
@@ -264,7 +312,7 @@ class Parser:
                 cls.parse_term()
             # everything will fail unless its a valid and declared identifier
             elif (not cls.is_valid_identifier(token) or not cls.is_identifier_declared(token)):
-                raise ParserException
+                raise ParserException(_type, token)
 
 #------------------------------------------------------------------------
 
@@ -285,12 +333,15 @@ with open(filename, "r") as f:
 #------------------------------------------------------------------------
 
 # lexical analysis, generate tokens from source code
-try:
-    Tokenizer.CODE = code
-    Tokenizer.tokenize()
-except TokenizeException:
-    print("error")
-    sys.exit()
+# try:
+#     Tokenizer.CODE = code
+#     Tokenizer.tokenize()
+# except TokenizeException:
+#     print("error")
+#     sys.exit()
+
+Tokenizer.CODE = code
+Tokenizer.tokenize()
 
 # print("token done")
 # for x in range(100):
